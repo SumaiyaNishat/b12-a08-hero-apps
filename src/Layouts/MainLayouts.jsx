b8a4be1115c53e0fd55efda6d5f28ebee23bb69a
sumaiya-nishat <sumaiyanishat55@gmail.com>
2025-10-09 21:15:54 +0600

    setup with react router, tailwind and daisy ui

---
+++ src/Layouts/MainLayouts.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "../components/Header/Navbar";
+import { Outlet } from "react-router";
+import Footer from "../components/Footer/Footer";
+
+const MainLayouts = () =>{
+    return(
+        <div>
+            <Navbar></Navbar>
+            <Outlet></Outlet>
+            <Footer></Footer>
+
+        </div>
+    )
+}
+
+export default MainLayouts;
---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { createBrowserRouter } from "react-router";
+import MainLayouts from "../Layouts/MainLayouts";
+import App from "../pages/App";
+import Installation from "../pages/Installation";
+import ErrorPage from "../pages/ErrorPage";
+import Home from "../pages/Home";
+
+
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayouts/>,
+    errorElement: <ErrorPage/>,
+    children: [
+        {
+    index: true,
+    element: <Home/>,
+  },
+   {
+    path: "/app",
+    element: <App/>,
+  },    
+   {
+    path: "/installation",
+    element: <Installation/>,
+  },
+    ]
+  },
+  
+
+  
+]);

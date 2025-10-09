9c10ce54b3c0df613cf54f602258a0aacb5ebcbd
sumaiya-nishat <sumaiyanishat55@gmail.com>
2025-10-10 00:58:10 +0600

    added footer section

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -3,58 +3,71 @@ import { Link, NavLink } from "react-router";
 import imgLogo from "../../assets/logo.png";
 import { Github } from "lucide-react";
 
-
-const Navbar = () =>{
-    return(
-         <div className="navbar">
-        <div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+const Navbar = () => {
+  return (
+    <div className="navbar w-11/12 mx-auto ">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{" "}
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+          >
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="app">App</NavLink>
+            </li>
+            <li>
+              <NavLink to="installation">Installation</NavLink>
+            </li>
+          </ul>
+        </div>
+        <div className="flex gap-2 items-center">
+          <img src={imgLogo} alt="" className="w-10 h-10" />
+          <Link to="/" className="text-2xl text-[#632EE3] font-semibold">
+            HERO.IO
+          </Link>
+        </div>
       </div>
-      <ul
-        tabIndex={0}
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li>
-        <NavLink to='/'>Home</NavLink>
-      </li>
-      <li>
-        <NavLink to='app'>App</NavLink>
-      </li>
-      <li>
-        <NavLink to='installation'>Installation</NavLink>
-      </li>
-      </ul>
+      <div className="navbar-center">
+        <ul className="menu menu-horizontal px-1 hidden lg:flex text-xl items-center">
+          <li className="hover:text-[#9F62F2] hover:underline">
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li className="hover:text-[#9F62F2] hover:underline">
+            <NavLink to="/app">App</NavLink>
+          </li>
+          <li className="hover:text-[#9F62F2] hover:underline">
+            <NavLink to="/installation">Installation</NavLink>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <button className="btn bg-gradient-to-r from-[#632EE3] to-[#9F62F2] text-white rounded-lg">
+          {" "}
+          <Github /> Contribute
+        </button>
+      </div>
     </div>
-    <div className="flex gap-2 items-center">
-        <img src={imgLogo} alt="" className="w-10 h-10"/>
-    <Link to='/' className="text-2xl text-[#632EE3] font-semibold">HERO.IO</Link>
-    </div>
-  </div>
-  <div className="navbar-center">
-    <ul className="menu menu-horizontal px-1 hidden lg:flex text-xl items-center">
-      <li className="hover:text-[#9F62F2] hover:underline">
-        <NavLink to='/'>Home</NavLink>
-      </li>
-      <li className="hover:text-[#9F62F2] hover:underline">
-        <NavLink to='/app'>App</NavLink>
-      </li>
-      <li className="hover:text-[#9F62F2] hover:underline">
-        <NavLink to='/installation'>Installation
-        </NavLink>
-      </li>
-
-       
-    </ul>
-    
-   
-  </div>
-  <div className="navbar-end">
-    
-    <button className="btn bg-gradient-to-r from-[#632EE3] to-[#9F62F2] text-white rounded-lg"> <Github/> Contribute</button>
-  </div>
-</div>
-    )
-}
+  );
+};
 
 export default Navbar;
